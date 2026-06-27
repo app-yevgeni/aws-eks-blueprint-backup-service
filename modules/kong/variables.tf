8e0396602662a5720143032f4ae008ccc487172c
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-27 21:35:06 +0300

    update

--- modules/kong/variables.tf
+++ modules/kong/variables.tf
@@ -1,23 +1,23 @@
 variable "name" {
   description = "The name of the service/ingress"
   type        = string
-  default     = "httpd-server"
+  default     = "velero-ui"
 }
 
 variable "namespace" {
   description = "Namespace where the ingress and service are deployed"
   type        = string
-  default     = "default"
+  default     = "velero-ui"
 }
 
 variable "host" {
   description = "Hostname for ingress"
   type        = string
-  default     = "app.crypterio.co"
+  default     = "backup.appflex.io"
 }
 
 variable "service_port" {
   description = "Port of the backend service"
   type        = number
-  default     = "8080"
+  default     = "3000"
 }
